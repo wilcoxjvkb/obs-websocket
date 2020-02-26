9c2763fa85727a74668bbad6abbe89614fb234e4
Stéphane Lepin <stephane.lepin@gmail.com>
2020-02-26 15:43:50 +0100

    ci(linux): adapt scripts for azure and remove travis

--- CI/package-xenial.sh
+++ CI/package-xenial.sh
@@ -2,23 +2,21 @@
 
 set -e
 
-cd /root/obs-websocket
-
 export GIT_HASH=$(git rev-parse --short HEAD)
-export PKG_VERSION="1-$GIT_HASH-$TRAVIS_BRANCH-git"
+export PKG_VERSION="1-$GIT_HASH-$BRANCH_NAME-git"
 
-if [ -n "${TRAVIS_TAG}" ]; then
-	export PKG_VERSION="$TRAVIS_TAG"
+if [ "$BRANCH_FULL_NAME" =~ "^refs/tags/" ]; then
+	export PKG_VERSION="$BRANCH_NAME"
 fi
 
-cd /root/obs-websocket/build
+cd ./build
 
 PAGER=cat checkinstall -y --type=debian --fstrans=no --nodoc \
 	--backup=no --deldoc=yes --install=no \
 	--pkgname=obs-websocket --pkgversion="$PKG_VERSION" \
-	--pkglicense="GPLv2.0" --maintainer="contact@slepin.fr" \
+	--pkglicense="GPLv2.0" --maintainer="stephane.lepin@gmail.com" \
 	--pkggroup="video" \
 	--pkgsource="https://github.com/Palakis/obs-websocket" \
-	--pakdir="/package"
+	--pakdir="../package"
 
-chmod ao+r /package/*
+chmod ao+r ../package/*
